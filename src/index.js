f624192d611bb57e0cd9fb61fdf02bd2e13887c1
Tate Burns <tate.burns@lifeway.com>
2020-10-01 15:18:37 -0500

    feat: add index.js entry file

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Carousel from './components/Carousel';
+import SingleCarousel from './components/SingleCarousel';
+import ImageTile from './components/ImageTile';
+
+export {
+  Carousel,
+  SingleCarousel,
+  ImageTile
+};
