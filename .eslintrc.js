0ecd3fb18a50d8e9e69e6eea5a71a75baedf84d3
Tate Burns <tate.burns@lifeway.com>
2020-10-09 14:55:31 -0500

    chore: tweak eslint to lint all files and to handle typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,19 @@
+// known issue: import/extensions rule incorrectly warns on importing ts files without an extension (see https://github.com/benmosher/eslint-plugin-import/issues/1857))
+
 module.exports = {
   extends: [
     'corinth/browser',
+    'plugin:import/typescript'
   ],
+  ignorePatterns: [
+    'lib/**',
+    'node_modules/**'
+  ],
+  rules: {
+    'import/extensions': ['error', 'ignorePackages',
+      { js: 'never', jsx: 'never', ts: 'never', tsx: 'never' },
+    ]
+  },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
