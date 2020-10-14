86dee42aa17b626ba0817b130b890edff0ddb0c3
Tate Burns <tate.burns@lifeway.com>
2020-10-14 16:58:49 -0500

    refactor: move logic out of carousel into usePages hook

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -23,6 +23,7 @@ import {
   useIsInitialLayoutEffect
 } from '../hooks/layout';
 import { device, space, color } from '../utils/styleguide';
+import { usePages } from '../hooks/usePages';
 
 
 const Container = styled.div`
@@ -213,93 +214,6 @@ const pageRight = (container: OptionalHTMLElement) => {
   }
 };
 
-const getRawStartPoints = (tiles: HTMLElement[], targetWidth: number, targetOffset: TargetZoneOffsets, maxPosition: number): number[] => {
-  if (tiles.length < 1) {
-    return [];
-  }
-
-  const unboundedNextStart = leftEdgeOffset(tiles[0]) - targetOffset.left;
-  const nextStart = Math.min(unboundedNextStart, maxPosition);
-
-  const projectedTargetRightEdge = nextStart + targetOffset.left + targetWidth;
-  const offPageTiles = tiles.slice(1).filter(tile => rightEdgeOffset(tile) > projectedTargetRightEdge);
-
-  return [nextStart, ...getRawStartPoints(offPageTiles, targetWidth, targetOffset, maxPosition)];
-};
-
-const getRawEndPoints = (tiles: HTMLElement[], targetWidth: number, targetOffset: TargetZoneOffsets, minPosition: number): number[] => {
-  if (tiles.length < 1) {
-    return [];
-  }
-
-  const unboundedNextEnd = rightEdgeOffset(tiles[tiles.length - 1]) - targetOffset.left;
-  const nextEnd = Math.max(unboundedNextEnd, minPosition);
-
-  const projectedTargetLeftEdge = nextEnd + targetOffset.left - targetWidth;
-  const nextPagePoint = projectedTargetLeftEdge - targetOffset.left;
-  const offPageTiles = tiles.slice(0, -1).filter(tile => leftEdgeOffset(tile) < projectedTargetLeftEdge);
-
-  return [...getRawEndPoints(offPageTiles, targetWidth, targetOffset, minPosition), nextPagePoint];
-};
-
-const getScaledWeightedAverage = (list1: number[], list2: number[], leftRatio: number, rightRatio: number) => {
-  if (list1.length === 0) {
-    return [];
-  } else if (list1.length === 1) {
-    const ratio = (leftRatio + rightRatio) / 2;
-    return [list1[0] * (1 - ratio) + list2[0] * ratio];
-  }
-
-  const total = list1.length - 1;
-  const progress = (n: number) => n / total;
-  const ratio = (n: number) => (1 - progress(n)) * leftRatio + progress(n) * rightRatio;
-  return list1.map((item1, i) => (item1 * (1 - ratio(i)) + list2[i] * ratio(i)));
-};
-
-const getCurrentPage = (currentPosition: number, pageSplitPoints: number[]) => {
-  const pageIndex = pageSplitPoints.findIndex(splitPoint => currentPosition < splitPoint);
-  return pageIndex === -1 ? pageSplitPoints.length + 1 : pageIndex + 1;
-};
-
-
-type PagePoints = number[] | undefined;
-
-const calculatePagePoints = (container: HTMLElement): PagePoints => {
-  const tiles = getTiles(container);
-  const targetOffset = getTileTargetZoneOffsets(container);
-  const targetZoneWidth = container.clientWidth - targetOffset.left - targetOffset.right;
-
-  if (targetZoneWidth === 0) {
-    return undefined;
-  }
-
-  const maxPosition = container.scrollWidth - container.clientWidth;
-  const rawStartPoints = getRawStartPoints(tiles, targetZoneWidth, targetOffset, maxPosition);
-
-  const minPosition = targetZoneWidth;
-  const rawEndPoints = getRawEndPoints(tiles, targetZoneWidth, targetOffset, minPosition);
-
-  const pagePoints = getScaledWeightedAverage(rawEndPoints.slice(1), rawStartPoints.slice(0, -1), 0, .5);
-
-  return pagePoints;
-};
-
-
-const calculatePages = (container: HTMLElement, pagePoints: PagePoints) => {
-  if (pagePoints === undefined) {
-    return { total: 0, current: 0 };
-  }
-
-  const numberOfPages = pagePoints.length + 1;
-  const currentPosition = scrollPosition(container);
-  const currentPage = getCurrentPage(currentPosition, pagePoints);
-
-  return {
-    total: numberOfPages,
-    current: currentPage,
-  };
-};
-
 
 type Props = {
   selected?: number,
@@ -313,27 +227,7 @@ const Carousel = ({ selected = 0, onSelect = () => undefined, children, ...props
   const [onEdge, setOnEdge] = useState({ left: false, right: false });
   const noScroll = onEdge.left && onEdge.right;
 
-  const [pagePoints, setPagePoints] = useState<PagePoints>(undefined);
-  const [pages, setPages] = useState({ current: 0, total: 0 });
-
-  const updatePagePoints = () => {
-    const newPagePoints = containerRef.current
-      ? calculatePagePoints(containerRef.current)
-      : undefined;
-
-    setPagePoints(newPagePoints);
-    return pagePoints;
-  };
-
-  const updatePages = (pagePnts: PagePoints) => {
-    if (!containerRef.current) return;
-    const container = containerRef.current;
-
-    const newPages = calculatePages(container, pagePnts);
-    if (newPages.current !== pages.current || newPages.total !== pages.total) {
-      setPages(newPages);
-    }
-  };
+  const pages = usePages(containerRef);
 
   const updateTargetZoneOffset = () => {
     const newTargetOffset = containerRef.current
@@ -346,8 +240,7 @@ const Carousel = ({ selected = 0, onSelect = () => undefined, children, ...props
   };
 
   const updateLayoutProperties = () => {
-    const newPagePoints = updatePagePoints();
-    updatePages(newPagePoints);
+    pages.onLayoutUpdate();
     updateTargetZoneOffset();
   };
 
@@ -380,7 +273,7 @@ const Carousel = ({ selected = 0, onSelect = () => undefined, children, ...props
   };
 
   const onScroll: UIEventHandler = () => {
-    updatePages(pagePoints);
+    pages.onScroll();
   };
 
   const arrowKeyDown: KeyboardEventHandler = (e) => {
---
+++ src/hooks/usePages.ts
@@ -0,0 +1,144 @@
+import React, { useState } from 'react';
+import { getTargetZoneOffsets, leftEdgeOffset, rightEdgeOffset, scrollPosition, TargetZoneOffsets } from '../components/utils';
+
+/* TODO - generalize this? pass in parameter??? */
+const getTiles = (container: HTMLElement | undefined | null) =>
+  Array.from(container?.children ?? [])
+    .filter(child => !child.hasAttribute('data-carousel-skip')) as HTMLElement[];
+
+const getTileTargetZoneOffsets = (container: HTMLElement) => {
+  const tiles = getTiles(container);
+  return getTargetZoneOffsets(container, tiles);
+};
+
+const getRawStartPoints = (tiles: HTMLElement[], targetWidth: number, targetOffset: TargetZoneOffsets, maxPosition: number): number[] => {
+  if (tiles.length < 1) {
+    return [];
+  }
+
+  const unboundedNextStart = leftEdgeOffset(tiles[0]) - targetOffset.left;
+  const nextStart = Math.min(unboundedNextStart, maxPosition);
+
+  const projectedTargetRightEdge = nextStart + targetOffset.left + targetWidth;
+  const offPageTiles = tiles.slice(1).filter(tile => rightEdgeOffset(tile) > projectedTargetRightEdge);
+
+  return [nextStart, ...getRawStartPoints(offPageTiles, targetWidth, targetOffset, maxPosition)];
+};
+
+const getRawEndPoints = (tiles: HTMLElement[], targetWidth: number, targetOffset: TargetZoneOffsets, minPosition: number): number[] => {
+  if (tiles.length < 1) {
+    return [];
+  }
+
+  const unboundedNextEnd = rightEdgeOffset(tiles[tiles.length - 1]) - targetOffset.left;
+  const nextEnd = Math.max(unboundedNextEnd, minPosition);
+
+  const projectedTargetLeftEdge = nextEnd + targetOffset.left - targetWidth;
+  const nextPagePoint = projectedTargetLeftEdge - targetOffset.left;
+  const offPageTiles = tiles.slice(0, -1).filter(tile => leftEdgeOffset(tile) < projectedTargetLeftEdge);
+
+  return [...getRawEndPoints(offPageTiles, targetWidth, targetOffset, minPosition), nextPagePoint];
+};
+
+const getScaledWeightedAverage = (list1: number[], list2: number[], leftRatio: number, rightRatio: number) => {
+  if (list1.length === 0) {
+    return [];
+  } else if (list1.length === 1) {
+    const ratio = (leftRatio + rightRatio) / 2;
+    return [list1[0] * (1 - ratio) + list2[0] * ratio];
+  }
+
+  const total = list1.length - 1;
+  const progress = (n: number) => n / total;
+  const ratio = (n: number) => (1 - progress(n)) * leftRatio + progress(n) * rightRatio;
+  return list1.map((item1, i) => (item1 * (1 - ratio(i)) + list2[i] * ratio(i)));
+};
+
+const getCurrentPage = (currentPosition: number, pageSplitPoints: number[]) => {
+  const pageIndex = pageSplitPoints.findIndex(splitPoint => currentPosition < splitPoint);
+  return pageIndex === -1 ? pageSplitPoints.length + 1 : pageIndex + 1;
+};
+
+
+type PagePoints = number[] | undefined;
+
+const calculatePagePoints = (container: HTMLElement): PagePoints => {
+  const tiles = getTiles(container);
+  const targetOffset = getTileTargetZoneOffsets(container);
+  const targetZoneWidth = container.clientWidth - targetOffset.left - targetOffset.right;
+
+  if (targetZoneWidth === 0) {
+    return undefined;
+  }
+
+  const maxPosition = container.scrollWidth - container.clientWidth;
+  const rawStartPoints = getRawStartPoints(tiles, targetZoneWidth, targetOffset, maxPosition);
+
+  const minPosition = targetZoneWidth;
+  const rawEndPoints = getRawEndPoints(tiles, targetZoneWidth, targetOffset, minPosition);
+
+  const pagePoints = getScaledWeightedAverage(rawEndPoints.slice(1), rawStartPoints.slice(0, -1), 0, .5);
+
+  return pagePoints;
+};
+
+
+const calculatePages = (container: HTMLElement, pagePoints: PagePoints) => {
+  if (pagePoints === undefined) {
+    return { total: 0, current: 0 };
+  }
+
+  const numberOfPages = pagePoints.length + 1;
+  const currentPosition = scrollPosition(container);
+  const currentPage = getCurrentPage(currentPosition, pagePoints);
+
+  return {
+    total: numberOfPages,
+    current: currentPage,
+  };
+};
+
+type usePagesResult = {
+  current: number;
+  total: number;
+  onLayoutUpdate: () => void;
+  onScroll: () => void;
+};
+export const usePages = (containerRef: React.RefObject<HTMLElement>): usePagesResult => {
+  const [pagePoints, setPagePoints] = useState<PagePoints>(undefined);
+  const [pages, setPages] = useState({ current: 0, total: 0 });
+
+  const updatePagePoints = () => {
+    const newPagePoints = containerRef.current
+      ? calculatePagePoints(containerRef.current)
+      : undefined;
+
+    setPagePoints(newPagePoints);
+    return pagePoints;
+  };
+
+  const updatePages = (pagePnts: PagePoints) => {
+    if (!containerRef.current) return;
+    const container = containerRef.current;
+
+    const newPages = calculatePages(container, pagePnts);
+    if (newPages.current !== pages.current || newPages.total !== pages.total) {
+      setPages(newPages);
+    }
+  };
+
+  const onLayoutUpdate = () => {
+    const newPagePoints = updatePagePoints();
+    updatePages(newPagePoints);
+  };
+
+  const onScroll = () => {
+    updatePages(pagePoints);
+  };
+
+  return {
+    ...pages,
+    onLayoutUpdate,
+    onScroll
+  };
+};
